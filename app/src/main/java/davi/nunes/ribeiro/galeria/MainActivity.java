321b74803ad07942f77cbbddb035ae572645efed
20211TIIMI0028 <davinrgamer@gmail.com>
2023-05-05 09:02:40 -0300

    Initial commit

---
+++ app/src/main/java/davi/nunes/ribeiro/galeria/MainActivity.java
@@ -0,0 +1,29 @@
+package davi.nunes.ribeiro.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar); // coloca a toolbar como a actionbar padrao da tela
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+}
---
+++ app/src/main/java/davi/nunes/ribeiro/galeria/PhotoActivity.java
@@ -0,0 +1,31 @@
+package davi.nunes.ribeiro.galeria;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true); // habilita o botao de voltar na action bar
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
+}
